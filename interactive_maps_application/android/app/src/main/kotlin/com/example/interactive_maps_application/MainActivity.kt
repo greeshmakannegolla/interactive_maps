255790464552d542949fcaedca30a442cc496ffb
Greeshma <greeshma.kannegolla@remotestar.io>
2022-03-08 20:32:02 +0530

    Test push

---
+++ interactive_maps_application/android/app/src/main/kotlin/com/example/interactive_maps_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.interactive_maps_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ interactive_maps_application/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:interactive_maps_application/screens/home_page.dart';
+
+void main() async {
+  runApp(
+      const MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+}
